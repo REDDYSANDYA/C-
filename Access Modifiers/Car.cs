aae18b950b404582ae90b5f7a7967c714772e268
REDDYSANDYA <167607230+REDDYSANDYA@users.noreply.github.com>
2024-08-08 07:04:14 +0530

    Access_Modifiers

---
+++ Access Modifiers/Car.cs
@@ -0,0 +1,22 @@
+﻿namespace Access_Modifiers
+{
+    //Access modifier is a keyword, it is used to set the visibility for classess,fields,methods and properties.
+    //public keyword is accessable for all classess.
+    //private keyword is accessable for within the same class.
+    //protected is accessable for with the same class and is inherited from the other class.
+    //internal is accessable within its own assembly.
+
+    internal class Car
+    {
+        public string Color = "Red";
+
+        private string model = "Duster";
+        static void Main(string[] args)
+        {
+           Car car = new Car();
+            Console.WriteLine(car.Color);
+            Console.WriteLine(car.model);
+        }
+    }
+}
+
